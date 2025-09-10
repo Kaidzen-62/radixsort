22a710050d383a5607cf15d9cad88fc1d171c4a9
Kaidzen-62 <volumvolf@gmail.com>
2025-09-10 15:44:53 +0300

    optimized radixsort for uint32 type; fixed type in uint64 radixsort

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-
 TODO:
-[x] optimize uint64
-[ ] optimize uint32
-[ ] optimize uint16
-[ ] and integer (int8-16-32-64) version
-[ ] add interface version
-[ ] add string version
-[ ] add examples for godoc
-[ ] add comments for godoc
+- [x] optimize uint64
+- [x] optimize uint32
+- [ ] test whether the optimizations work for uint32 and uint16
+- [ ] optimize uint16
+- [ ] and integer (int8-16-32-64) version
+- [ ] add interface version
+- [ ] add string version
+- [ ] add examples for godoc
+- [ ] add comments for godoc
+
--- uint32.go
+++ uint32.go
@@ -1,58 +1,84 @@
 package radixsort
 
-// Uint32 sorts the data slice of type uint32 using radix sort algorithm with a temporary buffer.
+// Uint32 sorts the given slice of uint32 values in ascending order using the radix sort algorithm.
+// A temporary buffer (buf) is required, and its length must be at least as large as data.
 // It panics if buf is shorter than data slice.
+//
+// Both data and buf will be modified during sorting.
+// The algorithm is stable and runs in O(n) time complexity.
 func Uint32(data, buf []uint32) {
 	radix32b8(data, buf)
 }
 
-// radix32b8 performs radix sort on the data slice using the provided buffer.
-// The buffer must be at least as long as the data slice.
-// This function modifies both the data and buffer slices.
+// radix32b8 performs the internal radix sort implementation using 8-bit buckets.
+// The buffer length must be at least as large as data.
 func radix32b8(data, buf []uint32) {
 	if len(buf) < len(data) {
-		panic("Radixsort: radix32b8: len(buf) < len(data)")
+		panic("Radixsort: buffer length is less than data length")
 	}
 
-	if len(data) <= 1 {
-		return
-	}
-
-	// The buckets are first used to count element frequencies,
-	// and then reused to store offsets (prefix sums).
-	bucket := [4][256]uint{}
+	// offsets[d][b] stores prefix sums (insertion offsets) for digit d and offset b.
+	// First they are used as frequency counters, then converted into offsets.
+	offsets := [4][256]uint{}
 	for _, v := range data {
-		bucket[0][uint8(v>>(0*8))]++
-		bucket[1][uint8(v>>(1*8))]++
-		bucket[2][uint8(v>>(2*8))]++
-		bucket[3][uint8(v>>(3*8))]++
+		offsets[0][uint8(v>>(0*8))]++
+		offsets[1][uint8(v>>(1*8))]++
+		offsets[2][uint8(v>>(2*8))]++
+		offsets[3][uint8(v>>(3*8))]++
 	}
 
 	// Calculate offsets.
-	acc0 := bucket[0][0]
-	acc1 := bucket[1][0]
-	acc2 := bucket[2][0]
-	acc3 := bucket[3][0]
-	bucket[0][0] = 0
-	bucket[1][0] = 0
-	bucket[2][0] = 0
-	bucket[3][0] = 0
+	acc0 := offsets[0][0]
+	acc1 := offsets[1][0]
+	acc2 := offsets[2][0]
+	acc3 := offsets[3][0]
+	offsets[0][0] = 0
+	offsets[1][0] = 0
+	offsets[2][0] = 0
+	offsets[3][0] = 0
 	for i := 1; i < 256; i++ {
-		bucket[0][i], acc0 = acc0, acc0+bucket[0][i]
-		bucket[1][i], acc1 = acc1, acc1+bucket[1][i]
-		bucket[2][i], acc2 = acc2, acc2+bucket[2][i]
-		bucket[3][i], acc3 = acc3, acc3+bucket[3][i]
+		offsets[0][i], acc0 = acc0, acc0+offsets[0][i]
+		offsets[1][i], acc1 = acc1, acc1+offsets[1][i]
+		offsets[2][i], acc2 = acc2, acc2+offsets[2][i]
+		offsets[3][i], acc3 = acc3, acc3+offsets[3][i]
 	}
 
+	// Optimization: skip sorting passes where all elements in the digit are identical.
+	uniqueOffsets := [4]uint{}
+	for i := range 4 {
+		// If all offsets are the same, there is only one unique value.
+		if offsets[i][1] == offsets[i][255] {
+			uniqueOffsets[i] = 1
+			continue
+		}
+
+		for j := 1; j < 256; j++ {
+			if offsets[i][j-1] != offsets[i][j] {
+				uniqueOffsets[i]++
+			}
+			if offsets[i][j] == offsets[i][255] {
+				break
+			}
+		}
+	}
+
+	swaps := 0
 	src, dst := data, buf[:len(data)]
 	for i := range 4 {
+		if uniqueOffsets[i] < 2 {
+			continue
+		}
+		swaps++
+
 		for _, v := range src {
-			index := bucket[i][uint8(v>>(i*8))]
+			index := offsets[i][uint8(v>>(i*8))]
 			dst[index] = v
-			bucket[i][uint8(v>>(i*8))]++
+			offsets[i][uint8(v>>(i*8))]++
 		}
 		src, dst = dst, src
 	}
 
-	copy(data, src)
+	if swaps&1 == 1 {
+		copy(data, src)
+	}
 }
